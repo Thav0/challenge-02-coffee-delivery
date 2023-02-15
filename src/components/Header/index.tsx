363210d013c94104b294482365975b23e3edd48f
Gustavo Diniz <gustavo.diniz@pixelplan.net>
2023-02-15 21:27:33 +0000

    Header Update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,12 @@
 import { ReactComponent as Logo } from "@assets/logo.svg";
 import { ReactComponent as LocationIcon } from "@assets/icons/location.svg";
 import { ReactComponent as CartIcon } from "@assets/icons/cart.svg";
+import { useContext } from "react";
+import { CoffeeContext } from "../../context/CoffeeContext";
 
 export function Header() {
+  const { totalItems } = useContext(CoffeeContext);
+
   return (
     <header className="container mx-auto py-8 flex justify-between items-center">
       <Logo />
@@ -11,8 +15,11 @@ export function Header() {
           <LocationIcon className=" w-4 h-4" />
           <span className="ml-2 text-sm">Porto, Portugal</span>
         </div>
-        <div className="ml-3 p-2 rounded-md bg-yellow-light text-yellow-dark">
+        <div className="ml-3 p-2 rounded-md bg-yellow-light text-yellow-dark relative">
           <CartIcon className="w-4 h-4" />
+          <div className="flex items-center justify-center absolute -right-2.5 -top-2.5 w-5 h-5 bg-yellow-dark rounded-full">
+            <span className="text-xs text-white">{totalItems}</span>
+          </div>
         </div>
       </div>
     </header>
